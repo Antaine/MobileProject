a043dd138d88c743d6932b4e9f55ebc01413f032
Antaine <aconn@outlook.com>
2020-11-24 20:13:36 +0000

    Updated Camera Follow

--- Assets/__Scripts/CameraFollow.cs
+++ Assets/__Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     //Variables
-    public GameObject player;
+    public Transform player;
     private Vector3 offset;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,7 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z+player.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition,200*Time.deltaTime);
     }
 }
