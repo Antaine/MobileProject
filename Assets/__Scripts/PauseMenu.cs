483cad9d3ef8e83c51245db7d4c3410516bc3501
antaine <aconn@outlook.com>
2020-12-05 21:35:13 +0000

    Pause Menu 1.1

--- Assets/__Scripts/PauseMenu.cs
+++ Assets/__Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -35,6 +36,10 @@ public class PauseMenu : MonoBehaviour
         gameMenuUI.SetActive(false);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitGame(){
