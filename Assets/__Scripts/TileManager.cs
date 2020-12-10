2ee669f937f5e259ba6c71b10453f4e2c439469d
antaine <aconn@outlook.com>
2020-12-10 17:50:01 +0000

    Shield Spawn Basic

--- Assets/__Scripts/TileManager.cs
+++ Assets/__Scripts/TileManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
+    public GameObject shield;
     public float zSpawn = 0;
     public float tileLength =10;
     public int numberOfTiles = 10;
+    public float shieldRate = 5;
+    private int count =0;
     private List<GameObject> activeTiles = new List<GameObject>();
+    private List<GameObject> activeShields = new List<GameObject>();
 
     public Transform playerTransform;
     // Start is called before the first frame update
@@ -20,8 +24,13 @@ public class TileManager : MonoBehaviour
                SpawnTile(0);
                i++;
            }
+           else if(i%shieldRate == 0)
+           {
+                //DeleteShields();
+                SpawnShield();   
+           }
 
-            SpawnTile(Random.Range(0,tilePrefabs.Length));       
+            SpawnTile(Random.Range(1,tilePrefabs.Length));       
        }
     }
 
@@ -31,6 +40,14 @@ public class TileManager : MonoBehaviour
         if(playerTransform.position.z - 35 >zSpawn-(numberOfTiles * tileLength)){
             SpawnTile(Random.Range(0,tilePrefabs.Length));
             DeleteTile();
+            count++;
+        }
+
+        if(count == shieldRate)
+        {
+            SpawnShield();
+            DeleteShields();
+            count =0;
         }
     }
 
@@ -40,8 +57,19 @@ public class TileManager : MonoBehaviour
         zSpawn +=tileLength;
     }
 
+    public void SpawnShield(){
+        GameObject go = Instantiate(shield,transform.forward * zSpawn,transform.rotation);
+        activeShields.Add(go);
+        go.transform.position =  new Vector3(0,1,zSpawn);
+    }
+
     public void DeleteTile(){
         Destroy(activeTiles[0]);
         activeTiles.RemoveAt(0);
+    }
+
+    public void DeleteShields(){
+       // Destroy(activeShields[0]);
+        activeShields.RemoveAt(0);
     }
 }
