b052040c28f7ed35b47dcce6dbb41bc0ebde0b12
antaine <aconn@outlook.com>
2020-12-11 15:42:41 +0000

    Level Select & Basic Lobby

--- Assets/__Scripts/MainMenu.cs
+++ Assets/__Scripts/MainMenu.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour{
 
+public static int nextLevel = 1;
+public Text levelText;
 public void PlayGame()
 {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    nextLevel = 0;
+}
+
+public void LoadLevel()
+{
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +nextLevel);
+}
+
+public void SetLevelOffice()
+{
+    nextLevel =1;
+    levelText.text = "Level: Office";
+}
+
+public void SetLevelKitchen()
+{
+    nextLevel = 2;
+    levelText.text = "Level: Kitchen";
+}
+
+public void SetLevelCity()
+{
+    nextLevel =3;
+    levelText.text = "Level: City";
 }
 
 public void QuitGame()
